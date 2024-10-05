6d321f27a4a60fd14560a9ca1133f915d4693be5
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2024-10-05 00:34:41 -0400

    code cleanup and refactoring

--- RoomMates/ContentView.swift
+++ RoomMates/ContentView.swift
@@ -85,24 +85,6 @@ struct ContentView: View {
     }
 }
 
-
-struct ProfileView: View {
-    @EnvironmentObject var authViewModel: AuthViewModel
-    
-    var body: some View {
-        Button(action: {
-            authViewModel.signOut()
-        }) {
-            Text("Log Out")
-                .frame(maxWidth: 300)
-                .padding()
-                .background(Color.red)
-                .foregroundColor(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-        }
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
